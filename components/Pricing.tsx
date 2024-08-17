46ebfcd8da957b689220a7c3884aedf1a657f4a0
Fallou24 <falloundiaye0221@gmail.com>
2024-08-17 19:27:17 +0000

    Pricing section design

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import { Button, Card, CardBody, CardHeader } from "@nextui-org/react";
+import React from "react";
 
 export default function Pricing() {
   return (
-    <section>Pricing</section>
-  )
+    <section>
+      <h1 className="font-bold md:text-6xl text-4xl mb-6 lg:w-1/2 lg:mx-auto">
+        Plans & Tarifs
+      </h1>
+      <p className="mb-12 lg:w-1/2 lg:mx-auto">
+        Choisissez le plan qui correspond le mieux à vos besoins.
+      </p>
+      <div className="flex flex-col md:flex-row max-w-4xl mx-auto gap-8">
+        <Card className="bg-[#18181B] text-white ">
+          <CardHeader className="flex-col">
+            <h2 className="font-bold text-3xl mb-2">Standard</h2>
+            <p className="opacity-60 text-sm  text-center">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit
+              dipisicing elit. Deserun. Deserunt
+            </p>
+          </CardHeader>
+          <CardBody className="flex-col items-center">
+            <h2 className="font-bold text-3xl mb-2"></h2>
+            <p className="flex items-center gap-4 mb-6">
+              <span className="font-bold text-3xl">0 CFA </span>
+              <span className="opacity-60">/ month • Shipping at 14$ / Kg</span>
+            </p>
+            <Button className="bg-primary-color text-white">Choisir</Button>
+          </CardBody>
+        </Card>
+
+        <Card className="bg-[#18181B] text-white ">
+          <CardHeader className="flex-col">
+            <h2 className="font-bold text-3xl mb-2">Premium</h2>
+            <p className="opacity-60 text-sm  text-center">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit
+              dipisicing elit. Deserun. Deserunt
+            </p>
+          </CardHeader>
+          <CardBody className="flex-col items-center">
+            <h2 className="font-bold text-3xl mb-2"></h2>
+            <p className="flex items-center gap-4 mb-6">
+              <span className="font-bold text-3xl">15 000 CFA </span>
+              <span className="opacity-60">/ month • Shipping at 8$ / Kg</span>
+            </p>
+            <Button className="bg-primary-color text-white">Choisir</Button>
+          </CardBody>
+        </Card>
+      </div>
+    </section>
+  );
 }
